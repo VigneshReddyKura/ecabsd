1ca1f10195bafbe70922f6e440c0dc6cc26855f0
Vignesh Reddy Kura <kvigneshreddy963@gmail.com>
2026-05-02 14:25:15 +0530

    fix: threshold 0.3 for FocalLoss, 100 epochs

--- train.py
+++ train.py
@@ -104,7 +104,7 @@ def train_one_epoch(model, loader, optimizer, criterion, device, gradient_clip):
         optimizer.step()
 
         total_loss += loss.item() * labels.size(0)
-        binary_preds = (pred >= 0.5).long().cpu().numpy()
+        binary_preds = (pred >= 0.3).long().cpu().numpy()
         all_labels.extend(labels.cpu().numpy().tolist())
         all_preds.extend(binary_preds.tolist())
 
@@ -133,7 +133,7 @@ def validate(model, loader, criterion, device):
         loss = criterion(pred, labels.float())
         total_loss += loss.item() * labels.size(0)
 
-        binary_preds = (pred >= 0.5).long().cpu().numpy()
+        binary_preds = (pred >= 0.3).long().cpu().numpy()
         all_labels.extend(labels.cpu().numpy().tolist())
         all_preds.extend(binary_preds.tolist())
 
