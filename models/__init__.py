9f7d8e71840b05c329eab79a91f997ecd868f6c4
amanigreeva <a.manigreeva@gmail.com>
2026-05-30 21:13:32 +0530

    fix: remove ChainEncoder import from models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,14 +5,12 @@ Equivariant Cross-Attention for Binding Site Detection
 
 from .ecabsd_v3_model import ECABSDModelV3, GCNEncoderV3, ECABSDModel
 from .cross_attention import CrossAttention
-from .encoder import ChainEncoder
 from .graph_construction import build_residue_graph
 
 __all__ = [
     "ECABSDModelV3",
-    "ECABSDModel",        # alias for backward compatibility
+    "ECABSDModel",
     "GCNEncoderV3",
     "CrossAttention",
-    "ChainEncoder",
     "build_residue_graph",
 ]
