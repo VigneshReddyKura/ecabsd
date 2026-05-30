4cefa9ae1f81f2863abec4fe1f0405ff5ef46f3a
amanigreeva <a.manigreeva@gmail.com>
2026-05-30 20:32:06 +0530

    fix: update models/__init__.py to V3 imports only

--- models/__init__.py
+++ models/__init__.py
@@ -3,12 +3,16 @@ ECABSD Models Package
 Equivariant Cross-Attention for Binding Site Detection
 """
 
-from .ecabsd_model import ECABSDModel, GCNEncoder, CrossAttention
+from .ecabsd_v3_model import ECABSDModelV3, GCNEncoderV3, ECABSDModel
+from .cross_attention import CrossAttention
+from .encoder import ChainEncoder
 from .graph_construction import build_residue_graph
 
 __all__ = [
-    "ECABSDModel",
-    "GCNEncoder",
+    "ECABSDModelV3",
+    "ECABSDModel",        # alias for backward compatibility
+    "GCNEncoderV3",
     "CrossAttention",
+    "ChainEncoder",
     "build_residue_graph",
 ]
